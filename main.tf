d2a2f20e5b410d3e025b615278067a2c65d5c4b1
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-11-28 15:36:10 +0530

    updated

--- main.tf
+++ main.tf
@@ -156,4 +156,14 @@ resource "aws_iam_policy" "policy"{
 })
    
 #   })
+  
+  
+  
+ 
+  resource "aws_kms_key" "a" {}
+
+resource "aws_kms_alias" "a" {
+  name          = "alias/my-key-alias"
+  target_key_id = aws_kms_key.a.key_id
+}
  }
